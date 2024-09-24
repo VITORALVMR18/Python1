6a084e1f02fe360b09bfc8804d8e9152f01449fe
VITORALVMR18 <vitor1810176@gmail.com>
2024-09-24 09:08:03 -0300

    .

---
+++ A02_INPUT.py
@@ -0,0 +1,13 @@
+nome = "Asdrubal"
+nome = input("Digite seu nome: ")
+
+print(nome)
+n1 = int(input("Digite um numero inteiro: "))
+n2 = input("Digite outro numero inteiro: ")
+n2 = int(n2)
+print(n1 + n2)
+
+sobrenome = input("Digite seu sobrenome: ")
+idade = input("Digite sua idade: ")
+print(sobrenome + idade)
+print(f"Seu nome é {nome} {sobrenome} e sua idade é {idade}")
