65024fe0a6ffd0b60c33db12568d4088046ca069
Vitor Alves Moreira <vitor1810176@gmail.com>
2024-09-24 08:31:57 -0300

    Git inicial

---
+++ A01_HELLO.py
@@ -0,0 +1,22 @@
+print("Olá, mundo!")
+a = 1
+b = 2
+
+print(a + b)
+
+texto = "Esta é uma frase"
+
+print(texto + "9")
+
+a = 15.9 
+
+print(a)
+
+aumento = True 
+print(aumento)
+
+media = 7,3
+novaMedia = 7,9 
+print(media)
+print(novaMedia)
+
