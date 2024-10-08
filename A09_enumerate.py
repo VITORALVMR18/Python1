6b88d9c08a687e59480fb00f02069a231d5f920f
VITORALVMR18 <vitor1810176@gmail.com>
2024-10-08 08:29:06 -0300

    .

---
+++ A09_enumerate.py
@@ -0,0 +1,14 @@
+# ENUMERATE
+
+for indice in "Ciencia de Dados":
+    print(f"{indice}")
+
+
+
+for indice in enumerate("Ciencia de Dados"):
+    print(f"{indice}")
+
+for indice, caractere in enumerate("Ciencia de Dados"):
+    print(f"{indice}: {caractere}")
+
+lista = ["Brasil", "EUA", "Inglaterra", "Italia", "França", "Alemanha"]
