2b444e5d8d84ec5449a4593492dc893f15d2452f
VITORALVMR18 <vitor1810176@gmail.com>
2024-10-08 07:56:33 -0300

    .

---
+++ A08A_EXERCICIO.py
@@ -0,0 +1,23 @@
+#ano = int(input("Digite um ano inteiro: "))
+#meses = ano * 12
+#dias = meses * 30
+#horas = dias * 24
+#minutos = horas * 60
+#segundos = minutos * 60
+
+#print(f"{ano} tem: {meses}, {dias}, {horas}, {minutos}, {segundos}")
+
+def exercicio():
+    ano = int(input("Digite um ano inteiro: "))
+    meses = ano * 12
+    dias = meses * 30
+    horas = dias * 24
+    minutos = horas * 60
+    segundos = minutos * 60
+
+    print(f"{ano} tem {meses} meses {dias} dias {horas} horas {minutos} minutos {segundos} segundos")
+
+
+exercicio()
+
+
