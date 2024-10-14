b6fbf9eccecec1ba93498622e4dd00bd84d071c2
VITORALVMR18 <vitor1810176@gmail.com>
2024-10-14 08:42:08 -0300

    .

---
+++ A13_arquivos.py
@@ -0,0 +1,35 @@
+# Manipulando arquivos texto
+import os
+
+# CRIANDO E ESCREVENDO ARQUIVOS TEXTO 
+with open('arquivo.txt', 'w')as arquivo
+arquivo.write('Olá, mundo\n')
+arquivo.write('Estou aqui\n')
+linhas = ["Brasil\n", "Argentina\n", "Italia"]
+arquivo.writelines(linhas)
+
+texto = "Aqui vai uma mensagem"
+with open('outro-arquivo.txt', 'w')as arquivo:
+    arquivo.write(texto)
+
+arquivo = open('mais-um-arquivo.txt', 'w')
+arquivo.write(texto + "\n")
+arquivo.write("outra linha" + "\n")
+arquivo.write("finalizei o texto" + "\n")
+arquivo.close
+
+# ABRINDO E LENDO ARQUIVOS
+with open('arquivo.txt', 'r')as arquivo:
+    for linha in arquivo:
+        print(linha)
+
+lista = []
+with open ('arquivo.txt', 'r')as arquivo:
+    linhas = arquivo.readlines()
+    print(linhas)
+    for linha in linhas:
+        lista.append(linha)
+    print(linhas)
+
+os.rename('arquivo.txt', 'excluir.txt')
+os.remove('excluir.txt')
